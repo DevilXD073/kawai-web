d6bec1eaf8f68d0e723fcf41e99b658cc041e9ae
DevilXD073 <vyomj0703@gmail.com>
2026-08-17 09:59:42 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+import SectionHeading from "@/components/SectionHeading";
+import Reviews from "@/components/Reviews";
+export default function ReviewsPage(){return <div className="py-16 md:py-20"><div className="container-k"><SectionHeading eyebrow="Community" title="Customer Reviews" subtitle="Genuine customer feedback will be published here as it becomes available."/><Reviews/></div></div>}
